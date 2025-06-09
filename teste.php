e75874713b2bcaa0f61a2b550aba0fff9007e0cd
joaovitorper <jovipepa.10@gmail.com>
2025-06-09 10:53:37 -0300

    first commit Atividade-php

---
+++ teste.php
@@ -0,0 +1,18 @@
+<?php
+// Comentário de linha com barra dupla
+# comentário de linha com cerquilha 
+
+/*
+comentário de bloco 
+Este tipo de comentário pode  ter varias linhas
+
+*/
+//declaração de variavél 
+$nome= "joão";
+
+// concatenação e saídas de dados
+echo "ola " .$nome. "!<br";
+
+echo" olá Mundo!<br>"; // Saída com echo
+print("olá"); // saída com print
+?>
