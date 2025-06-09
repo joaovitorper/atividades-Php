6459a1dad6a0340821793cce0b899b0f1b2d3fb0
joaovitorper <jovipepa.10@gmail.com>
2025-06-09 11:46:27 -0300

    atualiza: inserie teste 02 e 03

---
+++ teste 03.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estruturas de controle</title>
+</head>
+<body>
+    <h1>Switch/ case</h1>
+    o valor é
+    <?php
+    $valor= 1;
+    switch ($valor) {
+        case 1:
+        echo "um";
+        break;
+        case 2:
+        echo "dois";
+        break;
+        case 3:
+        echo "três";
+        break;
+        default:
+        echo "nao sei";
+        }
+        ?>
+    
+
+
+    
+</body>
+</html>
---
+++ teste01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis em PHP</title>
+</head>
+<body>
+    <h1>variavéis em PHP</h1>
+    <?php
+     echo $valor= "joão";
+     echo $valor. "<br><br>";
+
+     $valor=9;
+     $valor= $valor+1;
+     echo $valor;
+     ?>
+    
+</body>
+</html>
